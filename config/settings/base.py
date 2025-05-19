5981641373ae7a33d2e1fba4980827e7dce6b0bc
badinoko <badinoko07@gmail.com>
2025-05-19 18:05:39 +0300

    base

--- config/settings/base.py
+++ config/settings/base.py
@@ -45,6 +45,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'allauth.account.middleware.AccountMiddleware',  # Added
 ]
 
 ROOT_URLCONF = 'config.urls'
@@ -116,3 +117,16 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.2/ref/settings/ #default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# DJANGO ADMIN ALLAUTH
+# ------------------------------------------------------------------------------
+DJANGO_ADMIN_FORCE_ALLAUTH = False
+
+# ADMIN
+# ------------------------------------------------------------------------------
+# Django Admin URL. Default to "admin".
+ADMIN_URL = "admin/"
+
+# LOGGING
+# ------------------------------------------------------------------------------
